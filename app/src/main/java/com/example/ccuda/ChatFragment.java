07ee35b6e47f83b7bb2a750e346c94633b7896a9
yhb1834 <yhb1834@hanmail.net>
2021-10-29 05:36:37 +0900

    화면 변경 적용

--- app/src/main/java/com/example/ccuda/ChatFragment.java
+++ app/src/main/java/com/example/ccuda/ChatFragment.java
@@ -7,13 +7,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class ChatFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.chat, container, false);
+        return inflater.inflate(R.layout.fragment_chat, container, false);
     }
 }
