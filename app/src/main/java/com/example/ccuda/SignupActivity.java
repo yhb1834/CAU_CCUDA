0420e0b1af336be7c02382771cdf188ac12976dd
tmddus2 <tmddus2__@naver.com>
2021-10-29 22:40:51 +0900

    signup 유효성 검사 전

--- app/src/main/java/com/example/ccuda/SignupActivity.java
+++ app/src/main/java/com/example/ccuda/SignupActivity.java
@@ -2,13 +2,23 @@
 
 package com.example.ccuda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.example.ccuda.db.SignupRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignupActivity extends AppCompatActivity {
     private EditText email, password, name, passwordconfirmed;
@@ -34,6 +44,38 @@ public class SignupActivity extends AppCompatActivity {
                 final String UserPwd=password.getText().toString();
                 final String UserName=name.getText().toString();
                 final String PassCk=passwordconfirmed.getText().toString();
+
+                //System.out.println(UserEmail);
+                //System.out.println(UserPwd);
+                //System.out.println(UserName);
+                //System.out.println(PassCk);
+
+                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject=new JSONObject(response);
+                            boolean success=jsonObject.getBoolean("success");
+
+                            if(success){
+                                Toast.makeText(getApplicationContext(), "회원가입 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent=new Intent(SignupActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                           } else {
+                                Toast.makeText(getApplicationContext(), "회원가입 실패", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                SignupRequest signupRequest=new SignupRequest(UserEmail, UserPwd, PassCk, UserName, responseListener);
+                RequestQueue queue= Volley.newRequestQueue(SignupActivity.this);
+                queue.add(signupRequest);
             }
         });
     }
