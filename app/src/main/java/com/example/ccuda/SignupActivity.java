dcd2e74818426976cc80f235dc18109fbf13c6f3
tmddus2 <tmddus2__@naver.com>
2021-10-29 10:59:20 +0900

    sign up 구현 중

--- app/src/main/java/com/example/ccuda/SignupActivity.java
+++ app/src/main/java/com/example/ccuda/SignupActivity.java
@@ -1,14 +1,41 @@
+// https://velog.io/@xyunkyung/안드로이드-회원가입-로그인-기능-구현하기-4
+
 package com.example.ccuda;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
+    private EditText email, password, name, passwordconfirmed;
+    private Button signup_button;
+    private AlertDialog dialog;
+    private boolean validate = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        email=findViewById(R.id.EmailAddress);
+        password=findViewById(R.id.Password);
+        name=findViewById(R.id.UserName);
+        passwordconfirmed=findViewById(R.id.PasswordConfirm);
+
+        signup_button=findViewById(R.id.signup);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String UserEmail=email.getText().toString();
+                final String UserPwd=password.getText().toString();
+                final String UserName=name.getText().toString();
+                final String PassCk=passwordconfirmed.getText().toString();
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/ccuda/db/SignupRequest.java
@@ -0,0 +1,28 @@
+// 회원가입 값 요청
+package com.example.ccuda.db;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.example.ccuda.Config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignupRequest extends StringRequest {
+    final static private String URL = String.format("%s/signup.php", Config.SERVER_URL);
+    private Map<String, String> parameters;
+
+    public SignupRequest(String UserEmail, String UserPwd, String UserName, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        parameters = new HashMap<>();
+        parameters.put("UserEmail", UserEmail);
+        parameters.put("UserPwd", UserPwd);
+        parameters.put("UserName", UserName);
+    }
+    public Map<String,String> getParams(){
+        return parameters;
+    }
+}
