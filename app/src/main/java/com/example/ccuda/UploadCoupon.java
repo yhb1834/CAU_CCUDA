a0cb6553bb2ada41c07811ba40c74780232160f5
tmddus2 <tmddus2__@naver.com>
2021-11-04 09:45:15 +0900

    크롤링 구현

--- app/src/main/java/com/example/ccuda/UploadCoupon.java
+++ app/src/main/java/com/example/ccuda/UploadCoupon.java
@@ -1,47 +1,55 @@
+// https://pyony.com/search/?page=50&event_type=&category=&item=100&sort=&q=
+// https://wonyong-jang.github.io/java/2020/07/01/Java-Selenium.html
+// https://todaycode.tistory.com/5
+// https://gdtbgl93.tistory.com/154
+// https://devjun.tistory.com/226?category=999558
+// http://daplus.net/java-android-os-networkonmainthreadexception를-수정하는-방법/
+// https://mizzo-dev.tistory.com/entry/Mac-OS-환경에서-Selenium-Driver-Path-설정하기
+
 package com.example.ccuda;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class UploadCoupon extends Fragment {
     FragmentTransaction transaction;
-    private String URL="http://gs25.gsretail.com/gscvs/ko/products/event-goods";
+
+    //private WebDriver driver=new ChromeDriver();
+    private String URL="https://pyony.com/search/";
+    public static final String WEB_DRIVER_PATH="/Users/iseung-yeon/Desktop/chromedriver";
+    public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(getContext(), "add button listener", Toast.LENGTH_SHORT).show();
-        new Thread(){
+        new Thread(new Runnable() {
+
             @Override
-            public void run(){
+            public void run() {
                 try{
-                    Document doc= Jsoup.connect(URL).get();
-                    Elements temele=doc.select(".prod_list");
-                    System.out.println(temele);
-                    boolean isEmpty=temele.isEmpty();
-                    if(isEmpty==false){
-                        Bundle bundle=new Bundle();
-                        bundle.putString("product",temele.get(0).text());
-                    }
-                } catch (Exception e){
+                    ArrayList<String> cuReturn=getProductList("cu","https://pyony.com/brands/cu/");
+                    ArrayList<String> gsReturn=getProductList("gs25","https://pyony.com/brands/gs25/");
+                    ArrayList<String> sevenReturn=getProductList("seven","https://pyony.com/brands/seven/");
+                }catch (Exception e){
                     e.printStackTrace();
                 }
             }
-        }.start();
+        }).start();
     }
 
     @Override
@@ -51,5 +59,26 @@ public class UploadCoupon extends Fragment {
         View v=inflater.inflate(R.layout.fragment_uploadcoupon, container, false);
 
         return v;
+    }
+
+    public ArrayList<String> getProductList(String convName, String url) throws IOException {
+        Document doc=Jsoup.connect(url).get();
+        Elements link=doc.select("a.page-link");
+        ArrayList<String> result=new ArrayList<String>();
+
+        String lastSize=link.get(link.size()-1).attr("href").replace("?page=","");
+        int lastPage=Integer.parseInt(lastSize);
+        for(int i=1;i<=lastPage;i++){
+            String URL=url+"?page="+Integer.toString(i);
+            Document docPage=Jsoup.connect(URL).get();
+            Elements elements=docPage.select("div.card-body.px-2.py-2");
+            for(Element element:elements){
+                String a=element.select("strong").text()+" "+element.select("span.badge.bg-"+convName).text()+" "+element.select("span.text-muted").text();
+                //System.out.println(a);
+                result.add(a);
+            }
+        }
+
+        return result;
     }
 }
