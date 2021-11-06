cc93dea05f7a698796b9a293a90eda2338a71101
wwll99 <wwll99@naver.com>
2021-11-06 23:04:50 +0900

    장바구니 삭제db 저장

--- app/src/main/java/com/example/ccuda/ui_Cart/CartFragment.java
+++ app/src/main/java/com/example/ccuda/ui_Cart/CartFragment.java
@@ -136,4 +136,22 @@ public class CartFragment extends Fragment {
 
         return MycartArrayList;
     }
+
+    // 장바구니 삭제 정보 db저장
+    protected void removeFromcart(int item_id){
+        Response.Listener<String> responsListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    // 성공
+                    Toast.makeText(context,"장바구니에서 삭제되었습니다.", Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        CartRequest cartRequest = new CartRequest("removeFromcart", SaveSharedPreference.getId(context), item_id, responsListener);
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(cartRequest);
+    }
 }
