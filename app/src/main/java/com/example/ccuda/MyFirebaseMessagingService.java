a67dc009e8d6e7422e36fe6237e1e6b722a53528
wwll99 <wwll99@naver.com>
2021-11-25 17:20:17 +0900

    FCM연동및이미지크기수정중

---
+++ app/src/main/java/com/example/ccuda/MyFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package com.example.ccuda;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        //token을 서버로 전송
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        //수신한 메시지를 처리
+    }
+}
