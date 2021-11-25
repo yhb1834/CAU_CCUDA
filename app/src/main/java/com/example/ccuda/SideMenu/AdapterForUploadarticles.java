e923dc1eb1d4c1258cc7622bfe83bea3191ed446
tmddus2 <tmddus2__@naver.com>
2021-11-25 16:50:39 +0900

    내가 올린 쿠폰 삭제 중

--- app/src/main/java/com/example/ccuda/SideMenu/AdapterForUploadarticles.java
+++ app/src/main/java/com/example/ccuda/SideMenu/AdapterForUploadarticles.java
@@ -1,14 +1,20 @@
 package com.example.ccuda.SideMenu;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Checkable;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 
@@ -24,18 +30,31 @@ public class AdapterForUploadarticles extends ArrayAdapter<CouponData> {
     ArrayList<CouponData> myUploadProductList=new ArrayList<CouponData>();
     private Context context;
     private ListView listView;
+    private LayoutInflater inflater;
+    private SparseBooleanArray mSelectedItemIds;
 
     private ImageView iconImageView;
     private TextView nameTextView;
     private TextView storeTextView;
     private TextView priceTextView;
     private TextView validityTextView;
+    ArrayList<View> viewArrayList=new ArrayList<>();
+
 
     public AdapterForUploadarticles(@NonNull Context context, int resource, ArrayList myUploadProductList) {
-        super(context, android.R.layout.simple_list_item_multiple_choice, myUploadProductList);
+        super(context, resource, myUploadProductList);
         this.context=context;
         this.myUploadProductList=myUploadProductList;
+        this.inflater=LayoutInflater.from(context);
         //this.myUploadProductList=objects;
+    }
+
+    private class ViewHolder{
+        private ImageView iconImageView;
+        private TextView nameTextView;
+        private TextView storeTextView;
+        private TextView priceTextView;
+        private TextView validityTextView;
     }
 
 
@@ -58,37 +77,106 @@ public class AdapterForUploadarticles extends ArrayAdapter<CouponData> {
         return position;
     }
 
+    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final int pos=position;
-        final Context context=parent.getContext();
+        //final Context context=parent.getContext();
+        final ViewHolder holder;
 
         if (convertView==null){
+            holder=new ViewHolder();
+            convertView=inflater.inflate(R.layout.product_list, null);
             //LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             //convertView=inflater.inflate(R.layout.product_list,parent,false);
-            convertView=LayoutInflater.from(getContext()).inflate(R.layout.product_list,parent,false);
-
+            //convertView=LayoutInflater.from(context).inflate(R.layout.product_list,parent,false);
+            holder.nameTextView=(TextView) convertView.findViewById(R.id.name);
+            holder.iconImageView=(ImageView) convertView.findViewById(R.id.icon);
+            holder.storeTextView=(TextView) convertView.findViewById(R.id.store);
+            holder.priceTextView=(TextView) convertView.findViewById(R.id.price1);
+            holder.validityTextView=(TextView) convertView.findViewById(R.id.validity1);
+            convertView.setTag(holder);
+            //convertView.setBackgroundColor(Color.parseColor("#FFC107"));
         }
-        nameTextView=(TextView) convertView.findViewById(R.id.name);
-        iconImageView=(ImageView) convertView.findViewById(R.id.icon);
-        storeTextView=(TextView) convertView.findViewById(R.id.store);
-        priceTextView=(TextView) convertView.findViewById(R.id.price1);
-        validityTextView=(TextView) convertView.findViewById(R.id.validity1);
+        else {
+            holder=(ViewHolder) convertView.getTag();
+        }
+        //nameTextView=(TextView) convertView.findViewById(R.id.name);
+        //iconImageView=(ImageView) convertView.findViewById(R.id.icon);
+        //storeTextView=(TextView) convertView.findViewById(R.id.store);
+        //priceTextView=(TextView) convertView.findViewById(R.id.price1);
+        //validityTextView=(TextView) convertView.findViewById(R.id.validity1);
 
         CouponData listViewProduct=myUploadProductList.get(position);
 
-        nameTextView.setText(listViewProduct.getItem_name());
+        holder.nameTextView.setText(listViewProduct.getItem_name());
         if(listViewProduct.getImageurl()=="" || listViewProduct.getImageurl()==null){
-            iconImageView.setImageResource(R.drawable.add);
+            holder.iconImageView.setImageResource(R.drawable.add);
         }else{
-            Glide.with(convertView).load(listViewProduct.getImageurl()).into(iconImageView);
+            Glide.with(convertView).load(listViewProduct.getImageurl()).into(holder.iconImageView);
         }
-        storeTextView.setText(listViewProduct.getStorename());
-        priceTextView.setText("  "+listViewProduct.getPrice() + " 원");
-        validityTextView.setText(listViewProduct.getExpiration_date());
+        holder.storeTextView.setText(listViewProduct.getStorename());
+        holder.priceTextView.setText("  "+listViewProduct.getPrice() + " 원");
+        holder.validityTextView.setText(listViewProduct.getExpiration_date());
 
+        viewArrayList.add(convertView);
         return  convertView;
 
         //return null;
     }
+
+    @Override
+    public void remove(CouponData object){
+        myUploadProductList.remove(object);
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<CouponData> getMyList(){
+        return myUploadProductList;
+    }
+
+    public void toggleSelection(int position){
+        if(mSelectedItemIds==null){
+            mSelectedItemIds=new SparseBooleanArray();
+            selectView(position, true);
+        }
+        else{
+            selectView(position, !mSelectedItemIds.get(position));
+        }
+
+
+
+
+    }
+
+    public void removeSelection(){
+        mSelectedItemIds=new SparseBooleanArray();
+        notifyDataSetChanged();
+    }
+
+    public void selectView(int position, boolean value){
+        if(value){
+            viewArrayList.get(position).setBackgroundColor(Color.parseColor("#FFC107"));
+            Toast.makeText(getContext(), position+" "+value, Toast.LENGTH_SHORT).show();
+            mSelectedItemIds.put(position, value);
+        }
+        else {
+            viewArrayList.get(position).setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+            Toast.makeText(getContext(), position+" "+value, Toast.LENGTH_SHORT).show();
+            mSelectedItemIds.delete(position);
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedCount(){
+        return mSelectedItemIds.size();
+    }
+
+    public SparseBooleanArray getSelectedIds(){
+        return mSelectedItemIds;
+    }
+
+
+
 }
